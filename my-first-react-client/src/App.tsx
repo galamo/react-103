6e83d3fd35b5afce84e389e5ff439901186a5cfb
galamo <galamouyal88@gmail.com>
2024-07-03 21:25:40 +0300

    a

--- my-first-react-client/src/App.tsx
+++ my-first-react-client/src/App.tsx
@@ -4,13 +4,13 @@ import { HowDoILookLike } from './components/how-do-i-look-like';
 import { JokesList } from './components/jokes';
 import { SingleJoke } from './components/jokes/single-joke';
 import { WhatIsYourName } from './components/whats-your-name';
-
+import css from "./style.module.css"
 export default function App() {
 
     return (
         <>
             <h1 style={{ color: "red" }} >Hello</h1>
-            <h1> Joke of the week</h1>
+            <h1 className={css.wili}> Joke of the week</h1>
             <SingleJoke description='aa' type='aa' jokeId={111} punchline='sss' />
             <WhatIsYourName />
             <HowDoILookLike />
