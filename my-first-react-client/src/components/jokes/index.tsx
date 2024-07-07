c02de719bdd35e7c79337d6f3ee916e49a9ececa
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2024-07-07 20:27:49 +0300

    a

--- my-first-react-client/src/components/jokes/index.tsx
+++ my-first-react-client/src/components/jokes/index.tsx
@@ -1,13 +1,32 @@
 import jokesData from "../../jokes.json";
 import { SingleJoke } from "./single-joke";
 import css from "./jokes.module.css";
+import { useState } from "react";
 
 type SingleJokeType = (typeof jokesData)[0];
 
 export function JokesList() {
+  const [filter, setFilter] = useState("");
+
+  const currentJokes = filter
+    ? jokesData.filter((joke) =>
+        joke.setup.toLowerCase().includes(filter.toLowerCase())
+      )
+    : jokesData;
+
   return (
     <div className={css.thisIsGreenBackground}>
-      {jokesData.map((singleJoke: SingleJokeType) => {
+      <div>
+        <input
+          type="text"
+          placeholder="search for joke"
+          onChange={(e) => {
+            setFilter(e?.target?.value?.toLowerCase());
+          }}
+        />
+        <h3> Result: {currentJokes.length} </h3>
+      </div>
+      {currentJokes.map((singleJoke: SingleJokeType) => {
         return (
           <SingleJoke
             punchline={singleJoke.punchline}
--- my-first-react-client/src/components/jokes/single-joke.tsx
+++ my-first-react-client/src/components/jokes/single-joke.tsx
@@ -9,13 +9,26 @@ type Props = {
 
 export function SingleJoke(props: Props) {
   const [youLikeThis, setYouLikeThis] = useState(false);
+  const [showPunchline, setShowPunchline] = useState(false);
 
   return (
     <div style={{ border: "1px dashed black" }}>
       <h2>{props.jokeId}</h2>
       <h3>😂</h3>
       <h2 style={{ background: "green" }}>{props.description}</h2>
-      <h3>{props.punchline}</h3>
+      <h3>
+        {showPunchline ? (
+          props.punchline
+        ) : (
+          <button
+            onClick={() => {
+              setShowPunchline(true);
+            }}
+          >
+            Show Punchline
+          </button>
+        )}
+      </h3>
       <h6>{props.type}</h6>
       <div>
         <button
