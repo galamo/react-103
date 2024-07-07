4c3017dd6836a58567b72471367809743d7b12fe
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2024-07-07 18:16:32 +0300

    a

--- my-first-react-client/src/components/jokes/index.tsx
+++ my-first-react-client/src/components/jokes/index.tsx
@@ -1,15 +1,22 @@
-import jokesData from "../../jokes.json"
-import { SingleJoke } from "./single-joke"
+import jokesData from "../../jokes.json";
+import { SingleJoke } from "./single-joke";
+import css from "./jokes.module.css";
 
-type SingleJokeType = typeof jokesData[0]
+type SingleJokeType = (typeof jokesData)[0];
 
 export function JokesList() {
-    return <div>
-        {jokesData.map((singleJoke: SingleJokeType) => {
-            return <SingleJoke punchline={singleJoke.punchline}
-                jokeId={singleJoke.id}
-                description={singleJoke.setup}
-                type={singleJoke.type} />
-        })}
+  return (
+    <div className={css.thisIsGreenBackground}>
+      {jokesData.map((singleJoke: SingleJokeType) => {
+        return (
+          <SingleJoke
+            punchline={singleJoke.punchline}
+            jokeId={singleJoke.id}
+            description={singleJoke.setup}
+            type={singleJoke.type}
+          />
+        );
+      })}
     </div>
+  );
 }
--- readme.md
+++ readme.md
@@ -1,20 +1,26 @@
 # EX1 - React
+
 1. Create 2 components in the same page App.tx
-2. `<WhatsYourName />` - will present your name 
+2. `<WhatsYourName />` - will present your name
 3. `<HowDoILookLike/>` - will show an image from web
 
+# EX2 -
 
-# EX2 - 
 1. split your components to DIFF files under components folder
 2. each component will have her own folder + tsx file.
 
-# EX3 - 
+# EX3 -
+
 1. Add iD + type to each joke in the UI
 
-
-
 # Homework 3.7
+
 - following our React Lesson, Create a `CountriesPage` Component
 - the page will show a list of all countries - image+country name
 - use import static json file with the data ( copy paste the data into `countries.json` file)
-- create a `SingleCountryCard` which recieve the relevant props and present the data 
+- create a `SingleCountryCard` which recieve the relevant props and present the data
+
+# Ex 1 - 7.6
+
+- Move all inline CSS from react-103/my-first-react-client/src/components/jokes/single-joke.tsx to
+  `singleJoke.module.css` file
