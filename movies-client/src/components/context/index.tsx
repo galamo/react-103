aeaf72d9791cf46b2328e3ea4f9bd6d0a01a32f3
galamo <galamouyal88@gmail.com>
2024-07-28 19:16:57 +0300

    a

--- movies-client/src/components/context/index.tsx
+++ movies-client/src/components/context/index.tsx
@@ -15,12 +15,34 @@ export const SettingsContext = createContext<SettingsType>({} as SettingsType)
 
 
 export function ContextWrapper({ children }: { children: any }) {
-    const [favorites, setFavorites] = useState([])
-    const [toggleTime, setToggleTime] = useState(false)
+    let favoritesInitialData = []
+    let timeInitialData = false;
+    try {
+        favoritesInitialData = JSON.parse(localStorage.getItem("favorites")) || []
+        let value = localStorage.getItem("time")
+        if (value === 'false') {
+            timeInitialData = false;
+        } else {
+            timeInitialData = true
+        }
+    } catch (error) {
+
+    }
+    const [favorites, setFavorites] = useState(favoritesInitialData as Array<MovieType>)
+    const [toggleTime, setToggleTime] = useState(timeInitialData)
+
+    function setFavoritesWrapper(favorites: Array<MovieType>) {
+        setFavorites(favorites)
+        localStorage.setItem("favorites", JSON.stringify(favorites))
+    }
+    function setToggleTimeWrapper(toggleTime: boolean) {
+        setToggleTime(toggleTime)
+        localStorage.setItem("time", toggleTime)
+    }
 
     return <>
-        <SettingsContext.Provider value={{ toggleTime, setToggleTime }}>
-            <FavoritesContext.Provider value={{ favorites, setFavorites }}>
+        <SettingsContext.Provider value={{ toggleTime, setToggleTime: setToggleTimeWrapper }}>
+            <FavoritesContext.Provider value={{ favorites, setFavorites: setFavoritesWrapper }}>
                 {children}
             </FavoritesContext.Provider>
         </SettingsContext.Provider>
